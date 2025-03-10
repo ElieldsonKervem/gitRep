03e989363b0979666b74fd0bd5033dad31deba3a
ElieldsonKervem <kervem.elieldson@academico.ifrn.edu.br>
2025-03-10 00:52:09 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+//vamos ver como funciona os principais comandos git
+//uso git config para configurar o meu git, quando eu instalo do git cria um arquivo no reposostiorio local para flagear os dados e verificar as mudançãs no codigo
+
+//usamos o comando git add  para adiconar um arquivo e adcionar os valores para serem passados para dentro do arquivo que verifica valores que foram alterados e podem adicionar tudo no meu git, git commit  serve para adiconar comentario ela passa de staged para preapador para ser enviado para repositorio online como github    
+
+const valores = [1,2,3,4,5];
+
